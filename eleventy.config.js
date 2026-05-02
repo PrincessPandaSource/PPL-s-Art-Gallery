df423488f48bae8476e5cddf0a875b8a38ed4853
PrincessPandaSource <Lisahuang2003@gmail.com>
2026-05-02 16:49:45 -0500

    Double pagination for categories and tags

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,4 +1,5 @@
 import { DateTime } from "luxon";
+import lodash from "lodash";
 
 export default function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("styles");
@@ -16,54 +17,77 @@ export default function(eleventyConfig) {
         return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toLocaleString(DateTime.DATE_FULL);
     });
 
-    eleventyConfig.addCollection("artCategories", function(collectionApi) {
-        let artCategories = new Array();
+    // Code source: https://www.codeflood.net/blog/2024/04/17/11ty-nested-pagination/
+    eleventyConfig.addCollection("artByCategories", function(collectionApi) {
         const artworks = collectionApi.getFilteredByTag("artGallery");
+
+        // Get categories
+        let artByCategories = new Array();
 
         artworks.forEach(artwork => {
             const artsCate = artwork.data.categories;
             artsCate.forEach(category => {
-                if (!(artCategories.includes(category))) artCategories.push(category);
+                if (!artByCategories[category]) artByCategories[category] = [];
+                artByCategories[category].push(artwork);
             });
         });
 
-        return artCategories;
+        // Page categories
+        let artByCategoriesPaged = [];
+        const pageSize = 20;
+        for (const category in artByCategories) {
+            // Reverse pages
+            artByCategories[category].sort((a, b) => a.date > b.date).reverse();
+
+            const totalPages = Math.ceil(artByCategories[category].length / pageSize);
+            
+            lodash.chunk(artByCategories[category], pageSize).forEach((artworks, index) => {
+                artByCategoriesPaged.push({
+                    category: category,
+                    artworks: artworks,
+                    pageNumber: index + 1,
+                    totalPages: totalPages
+                });
+            })
+        }
+
+        return artByCategoriesPaged;
     })
 
-    eleventyConfig.addCollection("artTags", function(collectionApi) {
-        let artTags = new Array();
+    eleventyConfig.addCollection("artByTags", function(collectionApi) {
         const artworks = collectionApi.getFilteredByTag("artGallery");
 
+        // Get tags
+        let artByTags = new Array();
+
         artworks.forEach(artwork => {
-            const artsTags = artwork.data.artTags;
+            const artsTags= artwork.data.artTags;
             artsTags.forEach(tag => {
-                if (!(artTags.includes(tag))) artTags.push(tag);
+                if (!artByTags[tag]) artByTags[tag] = [];
+                artByTags[tag].push(artwork);
             });
         });
 
-        return artTags;
-    })
+        // Page categories
+        let artByTagsPaged = [];
+        const pageSize = 20;
+        for (const tag in artByTags) {
+            // Reverse pages
+            artByTags[tag].sort((a, b) => a.date > b.date).reverse();
 
-    eleventyConfig.addFilter("filterByArtTag", function(artworks, type, tag) {
-        // Lowercase is used for consistency
-        tag = tag.toLowerCase();
+            const totalPages = Math.ceil(artByTags[tag].length / pageSize);
+            
+            lodash.chunk(artByTags[tag], pageSize).forEach((artworks, index) => {
+                artByTagsPaged.push({
+                    tag: tag,
+                    artworks: artworks,
+                    pageNumber: index + 1,
+                    totalPages: totalPages
+                });
+            })
+        }
 
-        const filteredArts = artworks.filter(artwork => {
-            let array;
-
-            switch (type) {
-                case "category":
-                    array = artwork.data.categories.map(c => c.toLowerCase());
-                    break;
-                case "tag":
-                    array = artwork.data.artTags.map(t => t.toLowerCase());
-                    break;
-            }
-
-            return array.includes(tag);
-        })
-
-        return filteredArts;
+        return artByTagsPaged;
     })
 }
 
